bc9febbca046d4996337f99ecd7270367f70ea6e
brandon park <brandonpark206@gmail.com>
2024-04-17 23:54:22 -0700

    add initial onboarding components

---
+++ rocketgames/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  rocketgames
+//
+//  Created by Brandon Park on 4/17/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onboarding View Here!")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
